132c479757d88ff7fb9ac3682b945a70ef8a7546
freevova <vgu@jetthoughts.com>
2016-05-31 18:58:41 +0300

    Add users info fields to migration

--- db/migrate/20160531151032_sorcery_core.rb
+++ db/migrate/20160531151032_sorcery_core.rb
@@ -1,9 +1,33 @@
 class SorceryCore < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
+      t.string  :email,            :null => false
+      t.string  :crypted_password
+      t.string  :salt
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :middle_name
+      t.boolean :remote
+
+      t.string  :location_city
+      t.string  :location_country
+
+      t.integer :contract_zip_code
+      t.string  :contract_street
+      t.string  :contract_city
+      t.string  :contract_country
+
+      t.integer :taxpayer_id
+      t.integer :register_number
+      t.date    :register_number_start_date
+      t.integer :c_a_number
+      t.string  :bank_name
+      t.integer :bank_zip_code
+      t.string  :bank_street
+      t.string  :bank_city
+      t.string  :bank_country
+      t.string  :swift_code
+      t.integer :iban_number
 
       t.timestamps
     end
