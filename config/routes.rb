15ad31712c76982b6dd541b4c02c4231f477bfeb
freevova <vgu@jetthoughts.com>
2016-06-01 15:44:18 +0300

    Add bootstrap
    Add bootstrap styles
    Delete useless files from views witch were generated with scaffold
    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :users
 
   get  'sign_up', to: 'users#new',             as: :sign_up
-  get  'log_in',  to: 'user_sessions#new',     as: :login
-  post 'log_out', to: 'user_sessions#destroy', as: :logout
+  get  'log_in',  to: 'user_sessions#new',     as: :log_in
+  post 'log_out', to: 'user_sessions#destroy', as: :log_out
 
   resources :password_resets, only: [:create, :edit, :update]
 end
