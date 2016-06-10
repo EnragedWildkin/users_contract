ae07e08db1cebf02a6fcdda359a624ad5d1ea10d
freevova <vgu@jetthoughts.com>
2016-06-10 11:06:12 +0300

    #7 Fix reset password ability
    Add ability to send email with reset password link
    Add style to reset password forms(edit and new)
    Add style to email message for reset password
    Add link to reset password form in login form
    Add default_url_options for development and prodaction environments
    Correct user name representation in all templates
    Transfer some words from views to locales

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get  'log_in',  to: 'user_sessions#new',     as: :log_in
   post 'log_out', to: 'user_sessions#destroy', as: :log_out
 
-  resources :password_resets, only: [:create, :edit, :update]
+  resources :password_resets, except: [:index, :destroy]
 
   resources :contracts, only: [:new] do
     get 'generate', on: :collection
