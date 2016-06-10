fcc868183670cc25d9078840882d685b5fff7069
freevova <vgu@jetthoughts.com>
2016-06-10 17:14:00 +0300

    #8 Add default contract start and end date
    Set default contract start and end date, as time now on admin's page

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -2,4 +2,8 @@ module ContractsHelper
   def locale_date(date, format, language)
     I18n.l( date, format: format, locale: language)
   end
+
+  def today_date
+    Time.now.strftime('%Y-%m-%d')
+  end
 end
