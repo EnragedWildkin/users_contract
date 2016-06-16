904acf705a0078a2bd641d2c1db8821e41539237
freevova <vgu@jetthoughts.com>
2016-06-16 15:29:25 +0300

    #2 Add SOR-LLC-draft-Ukraine
    Add ua.yml locale
    Change view of admins form for generating pdf-files
    Add SOR-LLC-draft-Ukraine template
    Add name methods for UserField model
    Fix mistaces in contract helper
    Split long text lines to a couple of rows in templates

--- app/models/user_field.rb
+++ app/models/user_field.rb
@@ -1,3 +1,15 @@
 class UserField < ActiveRecord::Base
   belongs_to :user, inverse_of: :en_user_field
+
+  def short_name
+    [last_name, first_name].join(' ')
+  end
+
+  def full_name
+    [last_name, first_name, middle_name].join(' ')
+  end
+
+  def name_with_initials
+    last_name + " " + [first_name[0], middle_name[0]].join('.')
+  end
 end
